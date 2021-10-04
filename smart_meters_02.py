d5ab25cf30ed97b4907497d976ed6db109407d6a
simonduane <37291359+simonduane@users.noreply.github.com>
2021-10-04 22:41:11 +0100

    more explanations, updated code
    The code is what produced the screenshots in the explanations.

--- smart_meters_02.py
+++ smart_meters_02.py
@@ -1,17 +1,51 @@
 """\
                             smart_meters.py
-version 02
+    version 02
 
-Reads smart meter data from a local copy of ovo "API" files and puts them into
-Python dictionaries. This facilitates processing which is robust when some data
-are missing. The first step in that processing is to populate numpy arrays.
-This is illustrated here by plotting usage, and converting gas usage from energy
-(in kWh) to metered units (in m^3).
-Rogue values for gas usage are cleaned. It looks like ovo uses/used the value
-(2^24-1)/1000, in m^3, as an indicator for bad or missing data. Not good enough.
+This module defines three functions and some demo code.
+
+
+    def read_cv(file="./calorific_value.csv"):
+
+Returns a dictionary of national grid calorific values. I copy-pasted these
+data from the NG website. I chose the data for the region where I live.
+
+
+    def read_ovo(name="2019-12", dataset="daily",
+                 path="./json/20WE/", verbose=False):
+
+Returns information from an ovo set of smart meter data as a dictionary. I have
+saved local copies of the datasets as files named
+    "./json/20WE/daily/2019-12.json"
+    "./json/20WE/half-hourly/2019-12-17.json"
+    etc.
+and the input dataset is selected by name.
+
+
+    def extend(s1, s2):
+
+Returns the smart dataset dictionary result of "merging" two existing smart
+dataset dictionaries.
+
+
+    def dt_from_dt64(dt64):
+
+Convert numpy datetime64 to python datetime - numpy datetime is better for use
+in dictionary keys, python datetime is better for time axis labelling in plots.
+
+
+    def bars(a, b):
+"Returns (2N,), (2N,) arrays, given (N+1,), (N,) arrays, for bar charts."
+
+
+    if __name__ == "__main__"
+
+Some examples of simple use...
 """
 import calendar
 import csv
+import datetime as dt
+import matplotlib.pyplot as plt
 import numpy as np
 import json
 import os
@@ -32,17 +66,20 @@ def read_cv(file="./calorific_value.csv"):  # copy-pasted from NG website
             cv[t] = round(float(v), 1)
     return cv
 
-def read_ovo(name, path, verbose=False):
+def read_ovo(name="2019-12", dataset="daily",
+             path="./json/20WE/", verbose=False):
     "Returns information from an ovo file of smart meter data as a dictionary."
-    f_name = path + name
+    if name[-4:] != "json":
+        name = name + ".json"
+    f_name = path + dataset + "/" + name
     if verbose:
-        print(f"reading {name} ...")
+        print(f"reading {f_name} ...")
     with open(f_name) as data:
         data_d = json.load(data)
-    minutes = np.timedelta64(1, "m")    # express all times in minutes
+    minutes = np.timedelta64(1, "m")   # this should be used as a "unit" of time
     hh = 30 * minutes
     day = 48 * hh
-    opening_time = np.datetime64(name[:-5], "m")
+    opening_time = np.datetime64(name[:-5], "m") # start of first interval
     if len(name) == 15:                 # eg "2019-12-17"
         duration = hh
         period = 48 * hh
@@ -55,7 +92,7 @@ def read_ovo(name, path, verbose=False):
     closing_time = opening_time + period
     all_times = np.arange(opening_time, closing_time, step=duration)
     smart = {'times': all_times}        # end times of all intervals in the day
-    for supply in data_d.keys():     # list the smart data, and make a dictionary
+    for supply in data_d.keys():    # list the smart data, and make a dictionary
         smart_list = data_d[supply]['data']
         smart[supply] = {}
         for s in smart_list:
@@ -63,7 +100,7 @@ def read_ovo(name, path, verbose=False):
             smart[supply][key] = s
     return smart
 
-def smart_extend(s1, s2):
+def extend(s1, s2):
     "Return a copy of data dictionary s1, extended to include data from s2."
     s = s1.copy()
     s['times'] = np.append(s1['times'], s2['times'])
@@ -71,30 +108,60 @@ def smart_extend(s1, s2):
         s[supply] = {**s1[supply], **s2[supply]}
     return s
 
+def dt_from_dt64(dt64):
+    "Convert numpy datetime64 to python datetime - better for matplotlib plots."
+    if type(dt64) != type(np.datetime64(0,"m")):
+        raise Exception(f"{dt64} has bad type {type(dt64)}")
+    s = str(dt64)
+    return dt.datetime(int(s[:4]),int(s[5:7]),int(s[8:10]),
+                       int(s[11:13]),int(s[14:16]))
+
+def bars(a, b):
+    "Returns (2N,), (2N,) arrays, given (N+1,), (N,) arrays, for bar charts."
+    x, y = [a[0]], [0.0]
+    for i,p in enumerate(b):
+        x += [a[i], a[i+1]]
+        y += [b[i], b[i]]
+    x += [a[-1]]
+    y += [0.0]
+    return np.array(x), np.array(y)
+
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
+# read in all my smart data:
     smart = {}
-    for data in ["daily", "half-hourly"]: # convert everything I have
-        f_path = (f"./json/20WE/{data}/")
-        f_all = os.listdir(f_path)
+    for data in ["daily", "half-hourly"]:
+        f_path = (f"./json/20WE/")
+        f_all = os.listdir(f_path + data)
         f_all.sort()
-        smart[data] = read_ovo(f_all[0], f_path)
-        s = read_ovo(f_all[1], f_path)
-        for f in f_all[2:]:
-            smart[data] = smart_extend(smart[data], s)
-            s = read_ovo(f, f_path)
-        smart[data] = smart_extend(smart[data], s)
+        smart[data] = read_ovo(f_all[0], data, f_path)
+        for f in f_all[1:]:
+            s = read_ovo(f, data, f_path)
+            smart[data] = extend(smart[data], s)
         print(f"{len(smart[data]['times'])} records of {data} data read ...")
 # plot the half-hourly data:
     data = "half-hourly"
     all_times = smart[data]['times']
+    datetimes = [dt_from_dt64(t) for t in all_times]
+    datetimes = [datetimes[0] - (datetimes[1] - datetimes[0])] + datetimes
     for supply in ['electricity', 'gas']:
         smart_data = smart[data][supply]
         all_data = np.array([smart_data[t]['consumption']
                              if t in smart_data else 0.0
                              for t in all_times])
+        units = "kWh"
         if supply == 'gas':
             all_data *= 3.6 / 39.5 / 1.02264 # convert from kWh to m^3
-            all_data = np.where(np.greater(all_data, 50), 0.0, all_data) # clean
-        plt.plot(all_times, all_data)
+            units = "m^3"
+# using the value (2^24-1)/1000 m^3 to flag up missing data is a BAD IDEA
+# have to enforce a physical limit, 6 m^3 per hour, which is a U6 meter limit:
+            all_data = np.where(np.greater(all_data, 50), 0.0, all_data)
+        all_data *= 48
+        units += " per day"
+        x, y = bars(datetimes, all_data)
+        fig, ax = plt.subplots()
+        ax.fill_between(x, y)
+        ax.set_title(f"{supply} consumption")
+        ax.set_ylabel(f"{units}")
+        ax.set_xlabel("date:time")
+        plt
         plt.show()
